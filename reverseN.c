fbfdf939e99a5c7bdcdbbf1959195de0831dfbf1
MELANCHOLY_WU <1666315117@qq.com>
2021-04-28 08:54:15 +0800

    1

--- reverseN.c
+++ reverseN.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
-int reverse( int number ){
-    int x = 0;
-    while(number!=0){
-        x = x*10+number%10;
-        number/=10;
+/* 
+求整数的逆序数，实例：输入：-12340,输出：-4321
+ */
+int reverse(int number)
+{
+    int x = 0;//保存结果
+    while (number != 0)//判断商是否等于0，如果不等于0则继续右移
+    {
+        x = x * 10 + number % 10;//结果先乘以10再加余数赋值给结果，舍去从低位到高位第一个非零位右边的0
+        number /= 10;
     }
     return x;
     
@@ -13,10 +18,8 @@ int reverse( int number ){
 int main()
 {
     int n;
-
     scanf("%d", &n);
     printf("%d\n", reverse(n));
-
     return 0;
 }
 
